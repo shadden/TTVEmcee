42b94747820c804afbc85bc74abd2f32343dd38e
Sam Hadden <shadden@u.northwestern.edu>
2014-09-26 09:27:19 -0500

    Allow runtime to be specified as a user option

--- ttv_ensemble_sampler.py
+++ ttv_ensemble_sampler.py
@@ -1,6 +1,5 @@
-
-#TTVFAST_PATH = "/projects/b1002/shadden/7_AnalyticTTV/03_TTVFast/PyTTVFast"
-TTVFAST_PATH = "/Users/samuelhadden/15_TTVFast/TTVFast/c_version/myCode/PythonInterface"
+TTVFAST_PATH = "/projects/b1002/shadden/7_AnalyticTTV/03_TTVFast/PyTTVFast"
+#TTVFAST_PATH = "/Users/samuelhadden/15_TTVFast/TTVFast/c_version/myCode/PythonInterface"
 
 import sys
 sys.path.insert(0, '/Users/samuelhadden/13_HighOrderTTV/TTVEmcee')
@@ -22,6 +21,7 @@ if __name__=="__main__":
 
 	parser = ArgumentParser(description='run an ensemble MCMC analysis of a pair of TTVs')
 	parser.add_argument('--restart', default=False, action='store_true', help='continue a previously-existing run')
+	parser.add_argument('--erase', default=False, action='store_true', help='Start walkers from old files but overwrite them')
 	parser.add_argument('-n','--nensembles', metavar='N', type=int, default=100, help='number of ensembles to accumulate')
 	parser.add_argument('--nacor', metavar='N', type=int, default=5, help='Number of consecutive non-infinite autocorellation measurements required before exiting')
 	parser.add_argument('--nwalkers', metavar='N', type=int, default=100, help='number of walkers to use')
@@ -101,6 +101,7 @@ if __name__=="__main__":
 		return nbody_fit.CoplanarParametersFitness(x) + logp(x)
 
 	def initialize_walkers(nwalk,p0):
+		"""Initialize walkers around point p0 = [ mass1,mass2,ex1,ey1,ex2,ey2,P2_obs/P1_obs, dL_obs]"""
 		ics = nbody_fit.GenerateRandomInitialConditions([p0[0],p0[1]],0.1,[[p0[2],p0[3]],[p0[4],p0[5]]],0.005,nwalk)
 		return array([ nbody_fit.convert_params(ic) for ic in ics])
 
@@ -119,8 +120,8 @@ if __name__=="__main__":
 		p=initialize_walkers(nwalkers,pars0)
 	
 	for x in p.reshape(-1,8):
-		assert logp(x)==0.0 , "Bad IC generated!"
-		#and fit(x) > -inf
+		assert logp(x)==0.0 and fit(x) > -inf, "Bad IC generated!"
+		
 
 	# initialize sampler
 	sampler = emcee.EnsembleSampler(nwalkers,ndim,fit,threads=nthreads)
@@ -135,7 +136,7 @@ if __name__=="__main__":
 	old_best_lnlike = None
 	reset = False
 	Nmeasured = 0
-	if not restart and not args.noloop:
+	if not restart or args.erase:
 		with gzip.open('chain.dat.gz', 'w') as out:
 				out.write("# Parameter Chains")
 		with gzip.open('chain.lnlike.dat.gz', 'w') as out:
@@ -145,9 +146,10 @@ if __name__=="__main__":
 # MAIN LOOP
 #------------------------------------------------
 
-	for k in range(100):
+	for k in range( int(ceil(nensembles/nthin)) ):
 		if args.noloop:
 			break
+		# take 'nthin' samples.
 		for p,lnlike,blobs in sampler.sample(p,iterations=nthin, storechain = True):
 			pass	
 
@@ -161,7 +163,8 @@ if __name__=="__main__":
 		
 		if old_best_lnlike is None:
 			old_best_lnlike = maxlnlike
-			
+			print 'Found best likelihood of {0:.1f}'.format(old_best_lnlike)
+
 		if maxlnlike >  old_best_lnlike + p.shape[-1]/2.0:
 			old_best_lnlike =  maxlnlike
 			print 'Found new best likelihood of {0:.1f}'.format(old_best_lnlike)
