3214aa304ee9f48149ee2fb6dcc3d9996149639d
Samuel Hadden <shadden1107@gmail.com>
2014-09-28 19:47:07 -0500

    =Added burn-in phase

--- ttv_ensemble_sampler.py
+++ ttv_ensemble_sampler.py
@@ -1,5 +1,5 @@
-TTVFAST_PATH = "/projects/b1002/shadden/7_AnalyticTTV/03_TTVFast/PyTTVFast"
-#TTVFAST_PATH = "/Users/samuelhadden/15_TTVFast/TTVFast/c_version/myCode/PythonInterface"
+#TTVFAST_PATH = "/projects/b1002/shadden/7_AnalyticTTV/03_TTVFast/PyTTVFast"
+TTVFAST_PATH = "/Users/samuelhadden/15_TTVFast/TTVFast/c_version/myCode/PythonInterface"
 
 import sys
 sys.path.insert(0, '/Users/samuelhadden/13_HighOrderTTV/TTVEmcee')
@@ -23,6 +23,7 @@ if __name__=="__main__":
 	parser.add_argument('--restart', default=False, action='store_true', help='continue a previously-existing run')
 	parser.add_argument('--erase', default=False, action='store_true', help='Start walkers from old files but overwrite them')
 	parser.add_argument('-n','--nensembles', metavar='N', type=int, default=100, help='number of ensembles to accumulate')
+	parser.add_argument('--nburn', metavar='N', type=int, default=200, help='number samples to use for burn-in')
 	parser.add_argument('--nacor', metavar='N', type=int, default=5, help='Number of consecutive non-infinite autocorellation measurements required before exiting')
 	parser.add_argument('--nwalkers', metavar='N', type=int, default=100, help='number of walkers to use')
 	parser.add_argument('--nthin', metavar='N', type=int, default=10, help='number of setps to take between each saved ensemble state')
@@ -40,7 +41,7 @@ if __name__=="__main__":
 #----------------------------------------------------------------------------------
 # Get input TTV data and remove outliers
 #----------------------------------------------------------------------------------
-	input_dataTR,input_data1TR = TrimData(input_data,input_data1,tol=2.5)
+	input_dataTR,input_data1TR = TrimData(input_data,input_data1,tol=2.3)
 	if len(input_dataTR) != len(input_data):
 		print "Removed %d transit(s) from inner planet:" %( len(input_data) - len(input_dataTR) )
 		for bad in set(input_data[:,0]).difference( set(input_dataTR[:,0]) ):
@@ -59,6 +60,7 @@ if __name__=="__main__":
 	nwalkers = args.nwalkers
 	nthin=args.nthin
 	nthreads=args.nthreads
+	nburn = args.nburn
 
 #----------------------------------------------------------------------------------
 	if args.parfile != None:
@@ -98,11 +100,28 @@ if __name__=="__main__":
 		return 0.0
 
 	def fit(x):
-		return nbody_fit.CoplanarParametersFitness(x) + logp(x)
+		# Masses must be positive
+		masses = array((x[0],x[3]))
+		bad_masses = any(masses < 0.0)
+		if bad_masses:
+			return -inf
+		
+		# Mean anomalies lie between -pi and pi
+		bad_angles = abs(x[-1]) > pi
+		if bad_angles:
+			return -inf
+		
+		# Eccentricities must be smaller than 1
+		exs,eys = array([x[1],x[4]]),array([x[2],x[5]])
+		bad_eccs = any(exs**2 +eys**2 >= 1.0)
+		if bad_eccs:
+			return -inf
+		
+		return nbody_fit.CoplanarParametersFitness(x)
 
 	def initialize_walkers(nwalk,p0):
 		"""Initialize walkers around point p0 = [ mass1,mass2,ex1,ey1,ex2,ey2,P2_obs/P1_obs, dL_obs]"""
-		ics = nbody_fit.GenerateRandomInitialConditions([p0[0],p0[1]],0.1,[[p0[2],p0[3]],[p0[4],p0[5]]],0.005,nwalk)
+		ics = nbody_fit.GenerateRandomInitialConditions([p0[0],p0[1]],0.1,[[p0[2],p0[3]],[p0[4],p0[5]]],0.002,nwalk)
 		return array([ nbody_fit.convert_params(ic) for ic in ics])
 
 	means=[]
@@ -145,11 +164,18 @@ if __name__=="__main__":
 #------------------------------------------------
 # MAIN LOOP
 #------------------------------------------------
+	print "Starting burning..."
+	for p,lnlike,blobs in sampler.sample(p, iterations=nburn, storechain = True):
+		pass	
+	
+	p = sampler.flatchain[argsort(-sampler.flatlnprobability)[:nwalkers]] 
+	sampler.reset()
 
 	for k in range( int(ceil(nensembles/nthin)) ):
 		if args.noloop:
 			break
 		# take 'nthin' samples.
+		print "Burn-in complete, starting main loop"
 		for p,lnlike,blobs in sampler.sample(p,iterations=nthin, storechain = True):
 			pass	
 
