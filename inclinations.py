d7c2b012dc0dc78f1903ba5bcb5adb1ab99e74a2
Samuel Hadden <shadden1107@gmail.com>
2014-12-01 15:54:15 -0600

    most of the routines have been migrated to a class in PyTTV_3D

--- inclinations.py
+++ inclinations.py
@@ -8,14 +8,37 @@ r2au = 0.004649
 def guassian_prior(inclinations, observed, sigma):
 	return -0.5*np.sum((inclinations - observed)**2/sigma**2)
 	
-def ImpactParametersPriors(inclinations, periods, b, rstar, mstar, sigma_b,sigma_rstar,sigma_mstar)
-	""" Priors based on formula (a/R_*) cos(i) = b """
+def ImpactParametersPriors(inclinations, periods, b, rstar, mstar, sigma_b,sigma_rstar,sigma_mstar):
+	""" 
+		Gaussian priors based on formula (a/R_*) cos(i) = b.  Assumes that fractional uncertainties
+		are sufficiently small that we can linearize cos(i)'s dependence on the parameter errors
+		and therefore add error contributions in quadrature.
+	"""
+	rstar *=  r2au
+	sigma_rstar *= r2au
+	
 	cosi = np.abs(np.cos(inclinations))
-	a = ( mstar * (period / 365.25)**2 )**(1./3.)
+	a = ( mstar * (periods/ 365.25)**2 )**(1./3.)
 	
-	cosi0 = np.abs( b * rstar * r2au / a )
+	cosi0 = np.abs( b * rstar / a )
 	
-	#dcosi ~ (1/3) * da/a + dr/r + db/b 
- 	sigma_cosi = np.sqrt( (sigma_rstar/rstar)**2 + (sigma_mstar/(3 *mstar) )**2 + (sigma_b/b)**2 )
- 	
+	#sigma_cosi ~  Sum [ d (cosi) /dq * sigma_q ]
+	dcos_dM = -(1./3.) * cosi0  / mstar
+	dcos_dr = b / a
+	dcos_db = rstar / a
+	
+ 	sigma_cosi = np.sqrt( (dcos_dr * sigma_rstar)**2 + (dcos_dM * sigma_mstar )**2 + (dcos_db * sigma_b )**2 )
+
  	return -0.5 * np.sum ( (cosi - cosi0)**2 / sigma_cosi**2 )
+
+def ImpactParametersToInclinations(periods, b, rstar, mstar):
+	""" 
+		Convert periods and impact parameters to inclinations for a given stellar radius and mass
+	"""
+	rstar *= r2au
+	
+	a = ( mstar * (periods/ 365.25)**2 )**(1./3.)
+	
+	cosi0 = np.abs( b * rstar / a )
+ 	
+ 	return np.arccos(cosi0)
